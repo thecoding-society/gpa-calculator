fdcd153202cdfada0003bcabd08f516c1f7415ad
aviiciii <laavesh1@gmail.com>
2023-02-12 12:24:44 +0530

    add subjects

---
+++ program.py
@@ -0,0 +1,48 @@
+
+class subject:
+
+    def __init__(self, credit, grade_point):
+        self.credit = credit
+        self.grade_point = grade_point
+
+    def __str__(self):
+        return "Credit: " + str(self.credit) + "\nGrade Point: " + str(self.grade_point)
+
+class gpa(subject):
+    no_of_subjects = 0
+
+    def __init__(self):
+        self.gpa = 0.0
+        self.numerator = 0.0
+        self.denominator = 0.0
+        self.sub=[]
+
+    def addsubject(self, credit, grade_point):
+        self.sub.append(subject(credit, grade_point))
+        self.no_of_subjects += 1
+
+        
+    
+
+    def display(self):
+        print("No of subjects: ", self.no_of_subjects)
+        print("GPA: ", self.gpa)
+    
+    def __str__(self):
+        return "No of subjects: " + str(self.no_of_subjects) + "GPA: " + str(self.gpa)
+    
+def main():
+    gpa1 = gpa()
+    gpa1.addsubject(2, 8)
+    gpa1.addsubject(3, 8)
+    gpa1.addsubject(3, 7)
+    gpa1.addsubject(4, 8)
+    gpa1.addsubject(2, 10)
+    gpa1.addsubject(4, 9)
+    gpa1.addsubject(4, 9)
+    gpa1.addsubject(3, 8)
+
+    gpa1.display()
+
+if __name__ == "__main__":
+    main()
