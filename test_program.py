53a5a9af368082b787d917701cb4b020b809d740
aviiciii <laavesh1@gmail.com>
2023-02-13 22:35:55 +0530

    boiler for test_program

---
+++ test_program.py
@@ -0,0 +1,50 @@
+import pytest
+from program import *
+
+def test_subject_class():
+    pass
+
+def test_gpa_class():
+    pass
+
+def test_addsubject():
+    pass
+
+def test_calc():
+    pass
+
+def test_calc_with_negative_credit():
+    pass
+
+def test_calc_with_negative_grade_point():
+    pass
+
+def test_calc_with_credit_greater_than_8():
+    pass
+
+def test_calc_with_grade_point_greater_than_10():
+    pass
+
+def test_calc_with_no_subjects():
+    pass
+
+def test_calc_with_gpa_greater_than_10():
+    pass
+
+def test_calc_with_gpa_less_than_10():
+    pass
+
+def test_calc_with_gpa_equal_to_10():
+    pass
+
+def test_invalid_credit():
+    pass
+
+def test_invalid_grade_point():
+    pass
+
+def test_invalid_credit_and_grade_point():
+    pass
+
+def test_proper():
+    pass
